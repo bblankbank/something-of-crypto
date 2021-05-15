9a6e8958027d26599a538e12f6f14e3a6cc7a3ee
Akkarawit Piyawin <aw.piyawin@gmail.com>
2021-05-15 14:05:14 +0700

    add beep

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,20 @@
-const queryForm1 = document.querySelector('.f1.query-form');
-const cbalance1 = document.querySelector('.f1.cbalance');
-const nbalance1 = document.querySelector('.f1.nbalance');
-const status1 = document.querySelector('.f1.status');
-// const reset1 = document.querySelector('.f1.reset-btn');
-const libalance1 = document.querySelector('.f1.li-balance');
+const beep = () => {
+    const audio = new Audio('sound/button-3.mp3');
+    audio.play();
+}
+    
+const f1_form = document.querySelector('.f1.query-form');
+const f1_cbalance = document.querySelector('.f1.cbalance');
+const f1_nbalance = document.querySelector('.f1.nbalance');
+const f1_status = document.querySelector('.f1.status');
+const f1_lbalance = document.querySelector('.f1.li-balance');
+const f1_spin = document.querySelector('.f1.spin');
 
-queryForm1.addEventListener('submit', async function(e) {
+f1_form.addEventListener('submit', async function(e) {
     e.preventDefault();
-    const address = queryForm1.elements.address.value;
-    const key = queryForm1.elements.key.value;
+    const address = f1_form.elements.address.value;
+    const key = f1_form.elements.key.value;
+    f1_spin.classList.toggle('d-none')
     const res = await checkAddressBalance(address, key);
 })
 
@@ -16,25 +22,26 @@ const checkAddressBalance = async (address, key) => {
     try {
         const config = {params: {module: 'account', action: 'balance', address: address, tax: 'lastest', apikey: key}};
         const res = await axios.get('https://api.bscscan.com/api', config);
-        if(cbalance1.innerText === '') {
-            cbalance1.innerText = res.data.result;
+        const result = (parseInt(res.data.result)/1000000000000000000).toFixed(18);
+        if(f1_cbalance.innerText === '') {
+            f1_cbalance.innerText = result;
             const newLi = document.createElement('li');
-            newLi.append(res.data.result);
-            libalance1.append(newLi);
+            newLi.append(result);
+            f1_lbalance.append(newLi);
         }
-        nbalance1.innerText = res.data.result;
-        if(cbalance1.innerText !== nbalance1.innerText) {
-            status1.innerText = 'balance change!!!';
-            cbalance1.innerText = nbalance1.innerText;
+        f1_nbalance.innerText = result;
+        if(f1_cbalance.innerText !== f1_nbalance.innerText) {
+            beep();
+            f1_status.innerText = 'balance change!!!';
+            f1_cbalance.innerText = result;
             const newLi = document.createElement('li');
-            newLi.append(res.data.result);
-            libalance1.append(newLi);
+            newLi.append(result);
+            f1_lbalance.append(newLi);
         }
 
         const delayCheckBalance = (address, key) => {
             return new Promise((resolve, reject) => {
                 setTimeout(() => {
-                    console.log('func: nextBalance');
                     checkAddressBalance(address, key);
                 }, 500)
             })
@@ -46,28 +53,24 @@ const checkAddressBalance = async (address, key) => {
     }
 }
 
-// reset1.addEventListener('click', () => {
-//     cbalance1.innerText = '';
-//     nbalance1.innerText = '';
-//     status1.innerText = '';
-// }) 
-
-status1.addEventListener('click', () => {
-    status1.innerText = '';
+f1_status.addEventListener('click', () => {
+    f1_status.innerText = '';
 })
 
-const queryForm2 = document.querySelector('.f2.query-form');
-const tabletrans2 = document.querySelector('.f2.table-trans');
-const statusCheck2 = document.querySelector('.form-check-input')
+const f2_form = document.querySelector('.f2.query-form');
+const f2_table = document.querySelector('.f2.table-trans');
+const f2_status = document.querySelector('.form-check-input');
+const f2_spin = document.querySelector('.f2.spin');
 
 const hashArr = [];
 
-queryForm2.elements.timestamp.value = Math.round(new Date().getTime() / 1000) - 86400;
+f2_form.elements.timestamp.value = Math.round(new Date().getTime() / 1000) - 86400;
 
-queryForm2.addEventListener('submit', async function(e) {
+f2_form.addEventListener('submit', async function(e) {
     e.preventDefault();
-    const address = queryForm2.elements.address.value;
-    const key = queryForm2.elements.key.value;
+    const address = f2_form.elements.address.value;
+    const key = f2_form.elements.key.value;
+    f2_spin.classList.toggle('d-none')
     const res = checkNewTrans(address, key);
 })
 
@@ -75,14 +78,15 @@ const checkStatus = async (hash, key, dom) => {
     try {
         const config = {params: {module: 'transaction', action: 'gettxreceiptstatus', txhash: hash, apikey: key}};
         const res = await axios.get('https://api.bscscan.com/api', config)
-        if(res.data.result.status) dom.innerHTML = `<svg viewBox="0 0 100 50" xmlns="http://www.w3.org/2000/svg"><circle cx="50" cy="20" r="10" fill="green" /></svg>`;
-            else {
+        if(res.data.result.status) {
+            beep();
+            dom.innerHTML = `<svg viewBox="0 0 100 50" xmlns="http://www.w3.org/2000/svg"><circle cx="50" cy="20" r="10" fill="green" /></svg>`;
+        } else {
                 dom.innerHTML = `<svg viewBox="0 0 100 50" xmlns="http://www.w3.org/2000/svg"><circle cx="50" cy="20" r="10" fill="red" /></svg>`;
 
                 const delayCheckStatus = (hash, key, dom) => {
                     return new Promise((resolve, reject) => {
                         setTimeout(() => {
-                            console.log('func: nextStatus');
                             checkStatus(hash, key, dom);
                         }, 1000)
                     })
@@ -98,13 +102,13 @@ const checkNewTrans = async (address, key) => {
     try{
         const config = {params: {module: 'account', action: 'tokentx', address: address, apikey: key, sort: 'asc'}};
         const res = await axios.get('https://api.bscscan.com/api', config);
-        const transArr = res.data.result.filter(tran => tran.tokenName === "Pancake LPs").filter(tran => tran.timeStamp > queryForm2.elements.timestamp.value)
+        const transArr = res.data.result.filter(tran => tran.tokenName === "Pancake LPs").filter(tran => tran.timeStamp > f2_form.elements.timestamp.value)
         if(!transArr.some(tran => hashArr.includes(tran.hash))) {
             const newTrans = transArr.filter(tran => !hashArr.includes(tran.hash))
             for(tran of newTrans) {
                 hashArr.push(tran.hash);
                 const newTr = document.createElement('tr');
-                tabletrans2.append(newTr);
+                f2_table.append(newTr);
                 const tdHash = document.createElement('td');
                 tdHash.innerHTML = `<a href='https://bscscan.com/tx/${tran.hash}' target='_blank'>${tran.hash}</a>`;
                 newTr.append(tdHash);
@@ -119,7 +123,7 @@ const checkNewTrans = async (address, key) => {
 
                 const tdStatus = document.createElement('td');
                 newTr.append(tdStatus);
-                if(statusCheck2.checked) {
+                if(f2_status.checked) {
                     const statusCode = checkStatus(tran.hash, key, tdStatus)
                 }
             }
@@ -127,7 +131,6 @@ const checkNewTrans = async (address, key) => {
         const delayCheckTrans = (address, key) => {
             return new Promise((resolve, reject) => {
                 setTimeout(() => {
-                    console.log('func: nextTrans');
                     checkNewTrans(address, key);
                 }, 500)
             })
